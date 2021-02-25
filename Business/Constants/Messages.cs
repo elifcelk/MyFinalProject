081ba0f169f1e52eeb99e630507fafb160605045
elifclk444@gmail.com <elifclk444@gmail.com>
2021-02-25 23:48:23 +0300

    24.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string ProductsListed="Ürünler listelendi";
+        public static string ProductCountOfCategoryError="Kategorideki ürün sayısını aştınız.";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
