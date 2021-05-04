3964f264fcc668a1d96b9968daa2cd8991a90ba2
elifclk444@gmail.com <elifclk444@gmail.com>
2021-05-04 19:37:56 +0300

    .

--- Core/Utilities/Security/Hashing/HashingHelper.cs
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -6,9 +6,11 @@ namespace Core.Utilities.Security.Hashing
 {
     public class HashingHelper
     {
-        public static void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+
+        public static void CreatePasswordHash
+            (string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using (var hmac= new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
@@ -20,10 +22,10 @@ namespace Core.Utilities.Security.Hashing
         {
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
-                var computedHash=passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
                 for (int i = 0; i < computedHash.Length; i++)
                 {
-                    if (computedHash[i]!=passwordHash[i])
+                    if (computedHash[i] != passwordHash[i])
                     {
                         return false;
                     }
@@ -31,7 +33,8 @@ namespace Core.Utilities.Security.Hashing
                 return true;
             }
 
-            
+
         }
     }
 }
+    
